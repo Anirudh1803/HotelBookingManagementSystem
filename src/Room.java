e8a98aa52a293b509ec53f79928b55f637a23c2e
aniru <as3155@srmist.edu.in>
2026-03-10 21:47:36 +0530

    UC2 - Room types with inheritance and static availability

---
+++ src/Room.java
@@ -0,0 +1,26 @@
+/**
+ * Abstract representation of a Room in the hotel.
+ */
+
+public abstract class Room {
+
+    protected String roomType;
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    public Room(String roomType, int beds, int size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq.ft");
+        System.out.println("Price per night: $" + price);
+    }
+}
